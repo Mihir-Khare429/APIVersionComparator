15b417c74eca5b652840e07c1e8c67d9923e5664
mihirkhare <Mihir.Khare@cimpress.com>
2023-07-24 11:12:56 +0530

    Testing deployment and CI CD

--- scripts/main.tf
+++ scripts/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
     bucket = "financialflows.terraform"
-    key    = "eu-west-1/comparator/prd/terraform.tfstate"
+    key    = "eu-west-1/comparator/stg/terraform.tfstate"
     region = "eu-west-1"
   }
 }
@@ -36,3 +36,41 @@ locals {
     Squad           = "Amarok"
   }
 }
+
+module "service_environment_stg" {
+  source = "git::ssh://git@gitlab.com/cimpress-technology/ff/infrastructure.ci.cd/modules/ecs-service.git//service_environment?ref=v3.0.1"
+
+  container_port      = 80
+  container_memory    = 500
+  container_variables = local.container_variables
+  container_version   = var.container_version
+
+  on_image_deploy = "bash ../deploy-image.sh"
+
+  scaling_capacity_max = 4
+  scaling_capacity_min = 0
+
+  service_cluster                    = "ecs-service-cluster-nonprod"
+  service_count                      = 1
+  deployment_maximum_percent         = 200
+  deployment_minimum_healthy_percent = 50
+
+  service_environment = "nonprd"
+  service_name        = "comparatorProductPrice"
+  service_region      = "eu-west-1"
+
+  ordered_placement_strategy = [
+    {
+      type  = "spread"
+      field = "attribute:ecs.availability-zone"
+    },
+    {
+      type  = "binpack",
+      field = "memory"
+    }
+  ]
+
+  tags = local.tags
+
+}
+
